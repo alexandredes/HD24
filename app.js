7b107bb7291b50eb01cdc5f0389d8ea33e6fe1f0
Alexandre Deschamps <des.alexandre@gmail.com>
2019-04-04 12:38:46 -0400

    PCSC

--- app.js
+++ app.js
@@ -3,11 +3,71 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express().use(bodyParser.json());
 const path = require('path');
+var pcsc = require('pcsclite');
+
+var pcsc = pcsc();
+
 const router = express.Router();
 
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+
+pcsc.on('reader', function(reader) {
+    
+    console.log('New reader detected', reader.name);
+ 
+    reader.on('error', function(err) {
+        console.log('Error(', this.name, '):', err.message);
+    });
+ 
+    reader.on('status', function(status) {
+        console.log('Status(', this.name, '):', status);
+        /* check what has changed */
+        var changes = this.state ^ status.state;
+        if (changes) {
+            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+                console.log("card removed");/* card removed */
+                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Disconnected');
+                    }
+                });
+            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+                console.log("card inserted");/* card inserted */
+                reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Protocol(', reader.name, '):', protocol);
+                        reader.transmit(new Buffer([0x00, 0xB0, 0x00, 0x00, 0x20]), 40, protocol, function(err, data) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log('Data received', data);
+                                alert(data);
+                                reader.close();
+                                pcsc.close();
+                            }
+                        });
+                    }
+                });
+            }
+        }
+    });
+ 
+    reader.on('end', function() {
+        console.log('Reader',  this.name, 'removed');
+    });
+});
+ 
+pcsc.on('error', function(err) {
+    console.log('PCSC error', err.message);
+    alert(err.message);
+});
 
 io.on('connection', function(socket){
   socket.on('product_update', function(data){
