d979a8ec9b205ea83380f5084537a934b3546f91
Alexandre Deschamps <des.alexandre@gmail.com>
2019-04-04 12:56:37 -0400

    Update

--- app.js
+++ app.js
@@ -14,59 +14,88 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 
+
+
+
+
+var pcsc = pcsc();
+// PC/SC interface.
 pcsc.on('reader', function(reader) {
-    
-    console.log('New reader detected', reader.name);
- 
-    reader.on('error', function(err) {
-        console.log('Error(', this.name, '):', err.message);
-    });
- 
-    reader.on('status', function(status) {
-        console.log('Status(', this.name, '):', status);
-        /* check what has changed */
-        var changes = this.state ^ status.state;
-        if (changes) {
-            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
-                console.log("card removed");/* card removed */
-                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Disconnected');
-                    }
-                });
-            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
-                console.log("card inserted");/* card inserted */
-                reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('Protocol(', reader.name, '):', protocol);
-                        reader.transmit(new Buffer([0x00, 0xB0, 0x00, 0x00, 0x20]), 40, protocol, function(err, data) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                console.log('Data received', data);
-                                io.emit('data_received', data);
-                                reader.close();
-                                pcsc.close();
-                            }
-                        });
-                    }
-                });
-            }
-        }
-    });
- 
-    reader.on('end', function() {
-        console.log('Reader',  this.name, 'removed');
-    });
+ console.log('Reader detected:', reader);
+ reader.on('error', function(err) {
+   console.log('Error(', reader.name, '):', err.message);
+ });
+ reader.on('status', function(status) {
+  console.log('Status(', reader.name, '):', status);
+// Check changes.
+var changes = this.state ^ status.state;
+if (changes) {
+// Card removed.
+if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+  console.log('Status(', reader.name, '): Card removed');
+  reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
+    if (err) {
+      console.log('Error(', reader.name, '):', err);
+    }
+    else {
+      console.log('Status(', reader.name, '): Disconnected');
+    }
+  });
+}
+// Card inserted.
+else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+  console.log('Status(', reader.name, '): Card inserted');
+  reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+    if (err) {
+      console.log('Error(', reader.name, '):', err);
+    }
+    else {
+      console.log('Protocol(', reader.name, '):', protocol);
+/*
+Read card UID: [0xFF, 0xCA, 0x00, 0x00, 0x00]
+UID is specified in the ISO 14443 T=CL transport protocol while APDU's are specified in the ISO 7816 application layer protocol.
+"Get Data Command" is defined in PCSC 3 v2. If your driver is PCSC v2 compliant, you can get UID using it:
+Class = 0xFF
+INS = 0xCA
+P1 = 0x00
+P2 = 0x00
+Le = 0x00 (return full length: ISO14443A single 4 bytes, double 7 bytes, triple 10 bytes, for ISO14443B 4 bytes PUPI, for 15693 8 bytes UID)
+Expected response: Data+SW1SW2
+*/
+var message = new Buffer([0xFF, 0xCA, 0x00, 0x00, 0x00]);
+reader.transmit(message, 40, protocol, function(err, data) {
+  if (err) {
+    console.log('Error(', reader.name, '):', err);
+  } else {
+/* buf.readUIntLE(offset, byteLength[, noAssert])
+Set noAssert to true to skip validation of value and offset. Defaults to false.
+*/
+var lastRead = data.readUIntBE(0, 6, true).toString(16);
+var post = {evento: lastRead};
+console.log('Status(', reader.name, '): Read:', data, ' toString:', lastRead);
+//emit
+io.emit('data_received', lastRead);
+}
+});
+}
+});
+}
+}
 });
  
+ reader.on('end', function() {
+  console.log('Status(', reader.name, '): Removed');
+
+// Release resources.
+reader.close();
+pcsc.close();
+});
+});
 pcsc.on('error', function(err) {
-    console.log('PCSC error', err.message);
+  console.log('Error( PCSC ): ', err);
 });
+
+
 
 io.on('connection', function(socket){
   socket.on('product_update', function(data){
